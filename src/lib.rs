7a6409d8afbc215a863737b6f6b3b79fdd6239c3
Sunghyeon Jo <ainta@naver.com>
2025-06-04 20:02:11 -0400

    impl ED25519-POD as introdction pod

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,23 @@
 #![feature(trait_upcasting)]
+use std::fmt;
+
+use pod2::middleware::TypedValue;
+
+pub mod ed25519pod;
+
+pub enum PodType {
+    Ed25519 = 1001,
+}
+
+impl fmt::Display for PodType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            PodType::Ed25519 => write!(f, "Ed25519"),
+        }
+    }
+}
+impl From<PodType> for TypedValue {
+    fn from(t: PodType) -> Self {
+        TypedValue::from(t as i64)
+    }
+}
